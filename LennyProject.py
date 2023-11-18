e115b55ae49d910637a5dc0f40a76838630d07c9
YeoStaph <151381803+YeoStaph@users.noreply.github.com>
2023-11-18 22:51:05 +0800

    Update LennyProject.py

--- LennyProject.py
+++ LennyProject.py
@@ -27,7 +27,7 @@ def captioner(image):
 
 def main():
     st.title("Image Captioning and Audio Playing App")
-    uploaded_image = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+    uploaded_image = st.camera_input("Upload an image")
 
     if uploaded_image is not None:
         try:
