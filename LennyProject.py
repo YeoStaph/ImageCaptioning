c0ef0dd8b066d8dfee20b37038a8f8c15204d1f2
YeoStaph <151381803+YeoStaph@users.noreply.github.com>
2023-11-18 22:44:23 +0800

    Update LennyProject.py

--- LennyProject.py
+++ LennyProject.py
@@ -22,27 +22,29 @@ def captioner(image):
         return caption, audio_path
     except Exception as e:
         # Log the exception to identify the issue
-        print(f"Error: {e}")
+        st.error(f"Error: {e}")
         return "Error", None
 
 def main():
     st.title("Image Captioning and Audio Playing App")
-    uploaded_image = st.camera_input("Upload an image")
+    uploaded_image = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
     if uploaded_image is not None:
-
-            caption, audio = captioner(Image.open(uploaded_image))
+        try:
+            caption, audio_path = captioner(Image.open(uploaded_image))
             # Perform image captioning
             st.write("Image Caption:", caption)
 
             # Play audio associated with the image
-            audio = open("output.mp3", "rb").read()
+            with open(audio_path, "rb") as audio_file:
+                audio = audio_file.read()
 
-            # Use st.markdown to embed HTML5 audio with autoplay
-            st.markdown(
-                f'<audio controls autoplay><source src="data:audio/mp3;base64,{base64.b64encode(audio).decode()}" type="audio/mp3"></audio>',
-                unsafe_allow_html=True,
-            )
+            # Use st.audio to embed audio
+            st.audio(audio, format="audio/mp3", start_time=0)
+
+        except Exception as e:
+            # Log the exception to identify the issue
+            st.error(f"An error occurred: {e}")
 
 if __name__ == "__main__":
     main()
